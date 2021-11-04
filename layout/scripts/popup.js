ce25a0e6c645e8c08e00724c370ee1e8839a2d1e
davidvidovic1990 <62121192+davidvidovic1990@users.noreply.github.com>
2021-11-04 18:43:40 +0100

    Add files via upload

---
+++ layout/scripts/popup.js
@@ -0,0 +1,32 @@
+const images = [...document.querySelectorAll('.image')];
+
+// popup
+
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close-btn');
+const imageName = document.querySelector('.image-name');
+const largeImage = document.querySelector('.large-image');
+const imageIndex = document.querySelector('.index');
+
+
+let index = 0; // will track our current image;
+
+images.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        updateImage(i);
+        popup.classList.toggle('active');
+    })
+})
+
+const updateImage = (i) => {
+    let path = `https://github.com/kunaal438/image-popup/blob/master/img/img${i+1}.png?raw=true`;
+    largeImage.src = path;
+    imageName.innerHTML = path;
+    imageIndex.innerHTML = `0${i+1}`;
+    index = i;
+}
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.toggle('active');
+})
+
